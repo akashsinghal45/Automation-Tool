466cac925379790133bbe77445ddc0a072e3827e
Akash Singhal <ASinghal2@xavient.com>
2018-02-06 15:02:45 +0530

    folder creation

---
+++ sharpRPA-0.0.9_Code/sharpRPA-0.0.9_Code/sharpRPA-0.0.9/README.md
@@ -0,0 +1,12 @@
+
+# sharpRPA
+# Please note this project is currently in an Alpha (preview) state and subject to change.
+sharpRPA is a Free, Open-Source, and Easy-To-Use RPA (Robotic Process Automation) solution built on the .NET Framework in C#.  sharpRPA allows you to automate straight-forward and repetative actions with ease - Don't Write Code, Build Functionality!
+
+![Main App Window](https://i.imgur.com/TCF8JNc.png)
+
+### Request a Feature
+Don't see an automation feature you are looking for?  [Create a new enhancement issue!](https://github.com/saucepleez/sharpRPA/issues/new)
+
+### Don't want to build your own?
+Click [here](https://github.com/saucepleez/sharpRPA/releases) to view the latest releases.  Release binaries are built in Visual Studio 'Release' mode and packaged into a .ZIP file.  Simply extract to a folder, double-click and run to get started.
---
+++ sharpRPA-0.0.9_Code/sharpRPA-0.0.9_Code/sharpRPA-0.0.9/sharpRPA/UI/CustomControls/frmAkash.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sharpRPA.UI.CustomControls
+{
+    public partial class frmAkash : Form
+    {
+      public static string strValue = "";
+        public frmAkash()
+        {
+           
+            InitializeComponent();
+
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+
+        private void frmAkash_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if (textBox1.Text != "")
+                strValue= textBox1.Text;
+
+            this.Hide();
+
+           
+        }
+        public string Data()
+        {
+            return strValue;
+        }
+    }
+}
